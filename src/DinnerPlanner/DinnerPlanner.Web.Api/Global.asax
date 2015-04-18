b46add9f2b5184d6c741595be679e5d6ef314a12
EgoDrama <it_david@hotmail.com>
2015-04-18 15:20:11 +0200

    Added initial projects in Visual Studio

---
+++ src/DinnerPlanner/DinnerPlanner.Web.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DinnerPlanner.Web.Api.WebApiApplication" Language="C#" %>
---
+++ src/DinnerPlanner/DinnerPlanner.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DinnerPlanner.Web.Startup))]
+namespace DinnerPlanner.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
